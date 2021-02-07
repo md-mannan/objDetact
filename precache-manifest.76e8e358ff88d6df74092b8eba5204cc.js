14501ac48cae8f296da4915801b15de72fd3e4f5
md-mannan <mdmannan580@gmail.com>
2021-02-07 13:34:15 +0300

    Object detection

---
+++ precache-manifest.76e8e358ff88d6df74092b8eba5204cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3745ba59fe182d6dc750e5a26cd89e25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8283c722609ac06e694",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "691e95c4a0a8138bc698",
+    "url": "/static/js/2.32cb657b.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/static/js/2.32cb657b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8283c722609ac06e694",
+    "url": "/static/js/main.2fa75f5f.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  }
+]);
